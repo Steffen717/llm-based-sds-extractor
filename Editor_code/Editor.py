61f38fa8c1449c21f038d399080d7ccd7056a694
Steffen <steffen.kades@stud-mail.uni-wuerzburg>
2025-06-10 08:45:08 +0200

    minsestzeilenhöhe

--- Editor_code/Editor.py
+++ Editor_code/Editor.py
@@ -14,7 +14,7 @@ from openpyxl.styles import PatternFill
 from excel_to_JSON import excel_to_json
 
 EXCEL_ROW_HEIGHT_PX = 15
-MIN_ROWS = 1
+MIN_ROWS = 2
 
 class ExcelViewer:
     def __init__(self, root):
